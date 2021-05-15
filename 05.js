d73fccfd2845d8269337e956f592d331153dd9b6
mkujun <kujundzic.marko@hotmail.com>
2021-05-15 15:38:48 +0200

    day 5 part 2

--- 05.js
+++ 05.js
@@ -4,6 +4,8 @@ let input = fs.readFileSync('input05.txt', 'utf8').split('\n').slice(0,-1);
 //let input = fs.readFileSync('testInput.txt', 'utf8').split('\n').slice(0,-1);
 let high = 0;
 
+let ids = [];
+
 input.forEach(row => {
   let rowsSplit = row.split('');
   let rows = [...Array(128).keys()];
@@ -24,12 +26,26 @@ input.forEach(row => {
     }
   })
 
-  //high = rows[0] * 8 + columns[0];
+  ids.push(rows[0] * 8 + columns[0]);
+
   if (rows[0] * 8 + columns[0] > high) {
     high = rows[0] * 8 + columns[0];
   }
 })
 
+// part 1
 console.log("high", high);
 
+// part 2
+let sortedIds = ids.sort(function(a,b) {
+  return a - b;
+})
 
+let testId = 27;
+sortedIds.forEach(id => {
+  if (id != testId) {
+    console.log("id", id);
+  }
+
+  testId++;
+})
