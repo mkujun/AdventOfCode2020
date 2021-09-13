b9987793836bee9dd1f21ce888fd50dd0ec2a51a
mkujun <kujundzic.marko@hotmail.com>
2021-09-13 09:59:15 +0200

    day 8 part 2

--- 08.js
+++ 08.js
@@ -8,10 +8,10 @@ let element;
 let split;
 let command;
 let value;
-let run = true;
+//let run = true;
+//let visited = new Array(input.length).fill(false);
 
-let visited = new Array(input.length).fill(false);
-
+/*
 do {
   element = input[nextJump];
   split = element.split(" ");
@@ -42,7 +42,6 @@ do {
       run = false;
     }
     else {
-      console.log("jmp indexOf", input.indexOf(input[nextJump]));
       visited[nextJump] = 1;
       nextJump = nextJump + parseInt(value);
     }
@@ -50,3 +49,74 @@ do {
 } while (run);
 
 console.log("acc", accumulator);
+*/
+
+// part 2
+
+// polje svih 'nop' indexa
+let noopsIndex = [];
+let jmpsIndex = [];
+
+input.forEach(row => {
+  split = row.split(" ");
+  command = split[0];
+  value = split[1];
+
+  if (command === "nop") {
+    noopsIndex.push(input.indexOf(row));
+  }
+  else if (command === "jmp") {
+    jmpsIndex.push(input.indexOf(row));
+  }
+})
+
+const part2 = (arr, index) => {
+  let row = arr[index];
+
+  if (index >= arr.length) {
+    console.log("accumulator", accumulator);
+    return true;
+  }
+
+  split = row.split(" ");
+  command = split[0];
+  value = split[1];
+
+  if (visited[index]) {
+    return false;
+  }
+
+  else if (command == "nop") {
+    visited[index] = true;
+    index++;
+    return part2(arr, index);
+  }
+
+  else if (command == "acc") {
+    visited[index] = true;
+    index++;
+    accumulator = accumulator + parseInt(value);
+    return part2(arr, index, accumulator);
+  }
+  else if (command == "jmp") {
+    visited[index] = true;
+    index = index + parseInt(value);
+    return part2(arr, index);
+  }
+}
+
+let visited = new Array(input.length).fill(false);
+
+jmpsIndex.forEach(i => {
+  let arr = [...input];
+
+  visited = new Array(input.length).fill(false);
+  accumulator = 0;
+
+  let replacement = arr[i];
+  let change = replacement.replace("jmp", "nop");
+
+  arr[i] = change;
+  
+  part2(arr, 0);
+})
