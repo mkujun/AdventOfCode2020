9cc0d74effd9d1f21f19abe614a1355ed69f58e2
mkujun <kujundzic.marko@hotmail.com>
2021-09-13 16:00:20 +0200

    day 9 part 1

---
+++ 09.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+let input = fs.readFileSync('input09.txt', 'utf8').split('\n').slice(0, -1);
+
+// helper function for checking that everyhing is undefined
+const everyIsUndefined = (arr) => {
+  let counter = 0;
+  arr.forEach(e => {
+    if (e === undefined) counter++;
+  })
+
+  return counter
+}
+
+const preamble = (beginIndex, endIndex) => {
+  let arrayForSearch;
+  let goal;
+
+  for(let i = beginIndex; i < endIndex; i++) {
+    arrayForSearch = input.slice(beginIndex, endIndex);
+    goal = parseInt(input[endIndex]);
+  }
+
+  const pero = twoSum(arrayForSearch, goal);
+  const counter = everyIsUndefined(pero);
+  
+  if (counter === 25) {
+    console.log("goal", goal);
+  }
+}
+
+const twoSum = (array, goal) => {
+  let mapOfNumbers = {};
+  let twoIndexes = [];
+
+  for (let i = 0; i < array.length; i++) {
+    mapOfNumbers[array[i]] = i;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    let target = goal - array[i];
+    if(mapOfNumbers[target] !== null && mapOfNumbers[target] !== i) {
+      twoIndexes.push(i);
+      twoIndexes.push(mapOfNumbers[target]);
+    }
+  }
+
+  return twoIndexes;
+}
+
+for (let i = 0; i < input.length; i++) {
+  preamble(i, i+25);
+}
