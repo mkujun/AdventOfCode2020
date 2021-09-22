8f77d865d6d114dc16b5150bd0c93d44ca54805e
mkujun <kujundzic.marko@hotmail.com>
2021-09-22 21:56:11 +0200

    day 12, part 2

--- 12.js
+++ 12.js
@@ -52,6 +52,7 @@ let rotateCompas = (currentDegree, direction, num, compas) => {
   }
 }
 
+/*
 input.forEach(e => {
   let row = splitAt(1)(e);
 
@@ -82,3 +83,107 @@ input.forEach(e => {
 })
 
 console.log("result", Math.abs(compas.west - compas.east) + Math.abs(compas.south - compas.north));
+*/
+
+// part 2
+let waypoint = {
+  "E": 10,
+  "W": 0,
+  "N": 1,
+  "S": 0
+}
+
+let ship = {
+  "E": 0,
+  "W": 0,
+  "N": 0,
+  "S": 0,
+}
+
+const rotateWaypoint = (waypoint, rotateDirection, rotateValue) => {
+  let waypointCopy = {...waypoint};
+
+  if (rotateDirection == "R") {
+    if (rotateValue == 90) {
+      // east => north
+      waypoint["E"] = waypointCopy["N"]
+
+      // south => east
+      waypoint["S"] = waypointCopy["E"]
+
+      // west => south
+      waypoint["W"] = waypointCopy["S"]
+
+      // north = > west
+      waypoint["N"] = waypointCopy["W"]
+    }
+    else if (rotateValue == 180) {
+      waypoint["E"] = waypointCopy["W"]
+      waypoint["S"] = waypointCopy["N"]
+      waypoint["W"] = waypointCopy["E"]
+      waypoint["N"] = waypointCopy["S"]
+    }
+    else if (rotateValue == 270) {
+      waypoint["E"] = waypointCopy["S"]
+      waypoint["S"] = waypointCopy["W"]
+      waypoint["W"] = waypointCopy["N"]
+      waypoint["N"] = waypointCopy["E"]
+    }
+  }
+  else if (rotateDirection == "L") {
+    if (rotateValue == 90) {
+      waypoint["E"] = waypointCopy["S"]
+      waypoint["S"] = waypointCopy["W"]
+      waypoint["N"] = waypointCopy["E"]
+      waypoint["W"] = waypointCopy["N"]
+    }
+    else if (rotateValue == 180) {
+      waypoint["E"] = waypointCopy["W"]
+      waypoint["S"] = waypointCopy["N"]
+      waypoint["N"] = waypointCopy["S"]
+      waypoint["W"] = waypointCopy["E"]
+    }
+    else if (rotateValue == 270) {
+      waypoint["E"] = waypointCopy["N"]
+      waypoint["S"] = waypointCopy["E"]
+      waypoint["N"] = waypointCopy["W"]
+      waypoint["W"] = waypointCopy["S"]
+    }
+  }
+}
+
+input.forEach(e => {
+  let row = splitAt(1)(e);
+
+  let direction = row[0];
+  let value = parseInt(row[1]);
+
+  if (direction == "F") {
+    ship["E"] = ship["E"] + waypoint["E"] * value;
+    ship["W"] = ship["W"] + waypoint["W"] * value;
+    ship["N"] = ship["N"] + waypoint["N"] * value;
+    ship["S"] = ship["S"] + waypoint["S"] * value;
+  }
+  else if (direction == "N") {
+    waypoint["N"] = waypoint["N"] + value;
+  }
+  else if (direction == "S") {
+    waypoint["S"] = waypoint["S"] + value;
+  }
+  else if (direction == "W") {
+    waypoint["W"] = waypoint["W"] + value;
+  }
+  else if (direction == "E") {
+    waypoint["E"] = waypoint["E"] + value;
+  }
+  else if (direction == "R") {
+    rotateWaypoint(waypoint, "R", value);
+  }
+  else if (direction == "L") {
+    rotateWaypoint(waypoint, "L", value);
+  }
+
+})
+console.log("waypoint", waypoint);
+console.log("ship", ship);
+console.log("result", Math.abs(ship.W - ship.E) + Math.abs(ship.S - ship.N));
